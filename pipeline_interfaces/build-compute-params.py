a783c65f7798acfe92a5c6b5c155600a6de7eb92
nsheff <nsheff@users.noreply.github.com>
2020-06-30 13:01:19 -0400

    extend salmon time

--- pipeline_interfaces/build-compute-params.py
+++ pipeline_interfaces/build-compute-params.py
@@ -18,7 +18,7 @@ params =   {
     "time": "04:00:00"}
 
 # These are ones that go quick
-fast_assets = ["fasta", "gencode_gtf", "ensembl_gtf", "ensembl_rb",
+fast_assets = ["fasta", "gencode_gtf", "ensembl_gtf", "ensembl_rb", "feat_annotation",
                "refgene_anno", "fasta_txome"]
 
 slow_assets = ["bismark_bt2_index", "bismark_bt1_index", "salmon_partial_sa_index"]
@@ -26,6 +26,8 @@ slow_assets = ["bismark_bt2_index", "bismark_bt1_index", "salmon_partial_sa_inde
 
 if args.asset in fast_assets:
     params['time'] = "00:30:00"
+    params['partition'] = "standard"
+    params['mem'] = "6000"
 
 if args.asset in slow_assets:
     params['time'] = "8:00:00"
@@ -44,6 +46,7 @@ if args.asset == 'bismark_bt1_index':
 
 if args.asset == 'salmon_partial_sa_index':
     params['mem'] = "96000"
+    params['time'] = "12:00:00"
 
 if args.asset == 'salmon_sa_index':
     params['mem'] = "72000"
