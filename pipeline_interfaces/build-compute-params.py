871025505ebc7f4725471f717c49ec205c20788b
nsheff <nsheff@users.noreply.github.com>
2020-09-16 15:25:41 -0400

    make build params output a compute namespace for new plugin system

--- pipeline_interfaces/build-compute-params.py
+++ pipeline_interfaces/build-compute-params.py
@@ -3,14 +3,14 @@
 import json
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description="Refgenie build params")
+parser = ArgumentParser(description="Refgenie build compute")
 
 parser.add_argument("-s", "--size", help="size", required=False)
 parser.add_argument("-a", "--asset", type=str, help="asset", required=True)
 
 args = parser.parse_args()
 
-params =   {
+compute = {
     "bulker_crate": "databio/refgenie:0.7.6",
     "mem": "24000",
     "cores": "1",
@@ -25,40 +25,40 @@ slow_assets = ["bismark_bt2_index", "bismark_bt1_index", "salmon_partial_sa_inde
 
 
 if args.asset in fast_assets:
-    params['time'] = "00:30:00"
-    params['partition'] = "standard"
-    params['mem'] = "6000"
+    compute['time'] = "00:30:00"
+    compute['partition'] = "standard"
+    compute['mem'] = "6000"
 
 if args.asset in slow_assets:
-    params['time'] = "8:00:00"
+    compute['time'] = "8:00:00"
 
 if args.asset == 'suffixerator_index':
-    params['mem'] = "32000"
+    compute['mem'] = "32000"
 
 if args.asset == 'bowtie2_index':
-    params['mem'] = "64000"
+    compute['mem'] = "64000"
 
 if args.asset == 'bismark_bt2_index':
-    params['mem'] = "64000"
+    compute['mem'] = "64000"
 
 if args.asset == 'bismark_bt1_index':
-    params['mem'] = "64000"
+    compute['mem'] = "64000"
 
 if args.asset == 'salmon_partial_sa_index':
-    params['mem'] = "112000"
-    params['time'] = "6:00:00"
-    params['cores'] = "8"
+    compute['mem'] = "112000"
+    compute['time'] = "6:00:00"
+    compute['cores'] = "8"
 
 if args.asset == 'dbnsfp':
-    params['time'] = "12:00:00"
+    compute['time'] = "12:00:00"
 
 if args.asset == 'salmon_sa_index':
-    params['mem'] = "72000"
+    compute['mem'] = "72000"
 
 if args.asset == 'star_index':
-    params['mem'] = "64000"
+    compute['mem'] = "64000"
 
 
-y = json.dumps(params)
+y = json.dumps({"compute": compute})
 
 print(y)
