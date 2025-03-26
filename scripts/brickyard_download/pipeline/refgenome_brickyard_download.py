6494fc7e803ab47a25218cae5b593473b239e906
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-03-26 14:27:04 -0500

    add basic pep and pipeline for downloading

---
+++ scripts/brickyard_download/pipeline/refgenome_brickyard_download.py
@@ -0,0 +1,10 @@
+import sys
+
+sample_name= sys.argv[
+    1
+]  
+ftp_file_path = sys.argv[2]
+# results_file = sys.argv[3]
+# schema_path = sys.argv[4]
+
+print(f"HERE IS THE FILE PATH:{ftp_file_path}")
