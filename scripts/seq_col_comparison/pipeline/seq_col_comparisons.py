90f529a692f407805dbfdead52e0f701f1d3b22c
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-04-08 07:35:36 -0500

    add seq col to pipeline

--- scripts/seq_col_comparison/pipeline/seq_col_comparisons.py
+++ scripts/seq_col_comparison/pipeline/seq_col_comparisons.py
@@ -3,8 +3,9 @@ import os
 import urllib.request
 import urllib.error
 import pipestat
-from refget import fasta_to_digest, fasta_to_seqcol_dict
+from refget import fasta_to_digest, fasta_to_seqcol_dict,fasta_to_seq_digests
 import pypiper
+import pprint
 
 sample_name= sys.argv[1]  
 digest = sys.argv[2]
@@ -16,3 +17,11 @@ print(f"Here is the file_path: {file_path}")
 exists = os.path.exists(file_path)
 print(f"File exists: {exists}")
 
+# WE WANT sha512t24u due to robustness wrt collisions
+
+# for x in fasta_to_seq_digests(file_path):
+#     print(f"{x.id}\t{x.length}\t{x.sha512t24u}\t{x.md5}")
+
+seq_dict = fasta_to_seqcol_dict(file_path)
+
+pprint.pprint(seq_dict)
