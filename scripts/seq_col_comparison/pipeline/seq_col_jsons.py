c4c86e92fd3f178586cef50aeab322da5c002ae1
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-04-16 10:48:58 -0600

    add sample level pipeline to seq col for jsons

---
+++ scripts/seq_col_comparison/pipeline/seq_col_jsons.py
@@ -0,0 +1,71 @@
+# takes fasta files, computes json of sequence collection to be used in downstream analysis
+# while we are here generate the chrom.sizes files too?
+
+# this will take a pep from pephub and MODIFY it with the new columns.
+
+
+import pypiper
+import os
+import sys
+import json
+from refget import fasta_to_seqcol_dict, SequenceCollection
+
+
+
+
+pep_config= sys.argv[1]   # {looper.pep_config} 
+looper_output_dir = sys.argv[2] # {looper.output_dir} 
+top_level_digest = sys.argv[3] # {sample.top_level_digest} 
+brickyard_location = sys.argv[4] # {sample.brickyard_location} 
+json_directory = sys.argv[5] # {sample.json_creation_location} 
+chrom_sizes_location = sys.argv[6] # {sample.chrom_sizes_location}
+sample_name = sys.argv[7]
+
+
+pypiper_logs = os.path.join(looper_output_dir, "pipeline_results",top_level_digest)
+
+pm = pypiper.PipelineManager(
+    name="FASTA_DOWLOADER",
+    outfolder=pypiper_logs,
+    pipestat_record_identifier=top_level_digest,
+    recover=True,
+)
+
+pm.start_pipeline()
+
+json_creation_path = os.path.join(json_directory,top_level_digest,".json")
+chrom_sizes_creation_path = os.path.join(chrom_sizes_location,top_level_digest,".chrom.sizes")
+
+
+
+# create seq_col
+
+res = SequenceCollection.from_dict(
+        fasta_to_seqcol_dict(brickyard_location),
+        inherent_attrs=["names", "sequences"],
+    )
+
+
+# write jsons
+level_2 = res.level2()
+with open(json_creation_path, "w") as f:
+    f.write(json.dumps(level_2 , indent=2))
+
+#write chrom.sizes
+name_length_pairs = level_2['name_length_pairs']
+with open(chrom_sizes_creation_path, 'w') as outfile:
+    for item in name_length_pairs:
+        line = f"{item['name']}\t{item['length']}\n"
+        outfile.write(line)
+
+# report final locations BACK to pephub
+psm = pipestat.PipestatManager(pephub_path=pep_config)
+
+result = psm.retrieve_one(record_identifier=sample_name)
+
+result.update({brickyard_json_path: json_creation_path})
+result.update({brickyard_chrom_sizes_path: chrom_sizes_creation_path})
+
+psm.report(record_identifier=sample_name,values=result)
+
+pm.stop_pipeline()
