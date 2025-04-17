3f145143ff0027089cb33a7b91ce15b3c33e699d
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-04-17 13:41:59 -0600

    add overlap_coefficient func

--- scripts/seq_col_comparison/pipeline/seq_col_comparisons.py
+++ scripts/seq_col_comparison/pipeline/seq_col_comparisons.py
@@ -3,12 +3,14 @@ import os
 import urllib.request
 import urllib.error
 import pipestat
-from refget import fasta_to_digest, fasta_to_seqcol_dict,fasta_to_seq_digests
+import json
+from refget import fasta_to_digest, fasta_to_seqcol_dict,fasta_to_seq_digests,compare_seqcols
 import pypiper
 import pprint
 import peppy
 from pephubclient import PEPHubClient
 from itertools import combinations
+from pprint import pprint
 
 #looper_config = sys.argv[1]  
 looper_config = "donaldcampbelljr/human_seqcol_digests_local:default"
@@ -16,48 +18,56 @@ looper_config = "donaldcampbelljr/human_seqcol_digests_local:default"
 # file_path = sys.argv[3]
 # looper_output_dir = sys.argv[4]
 
-# print(f"Here is the file_path: {file_path}")
-
-# exists = os.path.exists(file_path)
-# print(f"File exists: {exists}")
-
-# # WE WANT sha512t24u due to robustness wrt collisions
-
-# # for x in fasta_to_seq_digests(file_path):
-# #     print(f"{x.id}\t{x.length}\t{x.sha512t24u}\t{x.md5}")
-
-# seq_dict = fasta_to_seqcol_dict(file_path)
-
-# pprint.pprint(seq_dict)
-
 print(f"here is the looper config: {looper_config}")
 
+
+# Calculation Functions
+def calc_overlap_coeff(A,B, A_B_intersection):
+    minimum_value = min(abs(A),abs(B))
+    overlap = abs(A_B_intersection)/minimum_value
+    return overlap
+
+
+def calc_jaccard_similarity():
+    pass
+
+def calc_weighted_jaccard():
+    pass
 
 # initiate pephubclient object
 phc = PEPHubClient()
 pep = phc.load_project(looper_config)
 print(pep)
 
+# Retrieve all json file paths from the pep
+all_jsons = []
+key_digest_sample_name = {}
 
-# GET SEQ_COL_DICTS
-# Create a dictionary holding seq collection data for all the samples.
-# In the future, should potentially pull this from a database instead of calculating on the fly.
-seq_col_all={}
 for sample in pep.samples:
-    print(f"Here is the sample name: {sample.sample_name} here is top_level_digest: {sample.top_level_digest} Here is brickyard location: {sample.brickyard_location} ")
-    #seq_dict = fasta_to_seqcol_dict(sample.brickyard_location)
-    seq_dict = {}
-    seq_col_all.update({sample.top_level_digest: seq_dict})
-    #pprint.pprint(seq_dict)
+    all_jsons.append(sample.brickyard_json_path)
+    key_digest_sample_name.update({sample.top_level_digest:sample.sample_name}) # handy later when we need to make this more human readable
 
-# Once we have all the seq_col dictionaries computed, we can then do a pair-wise comparison
+print(all_jsons)
+print(key_digest_sample_name)
 
-all_digests_list = list(seq_col_all.keys())
+all_combinations = combinations(iterable=all_jsons,r=2)
 
-print(all_digests_list)
-
-all_combinations = combinations(iterable=all_digests_list,r=2) # r is for the number of elements in each combination. Just want pairwise for now.
-
-print("combination iterated")
 for combination in all_combinations:
-    print(combination)
+    json_fp_1=combination[0]
+    json_fp_2=combination[1]
+    with open(json_fp_1, "r") as f:
+        reloaded_dict1 = json.load(fp=f)
+    with open(json_fp_2, "r") as f:
+        reloaded_dict2 = json.load(fp=f)
+#
+    print(f"COMBINATION: {os.path.basename(json_fp_1)} vs {os.path.basename(json_fp_2)}")
+    # print(pprint(compare_seqcols(reloaded_dict1,reloaded_dict2),indent=4))
+    comparison = compare_seqcols(reloaded_dict1,reloaded_dict2)
+
+    # overlap for names
+    overlap_coefficient_names = calc_overlap_coeff(comparison['array_elements']['a']['names'],comparison['array_elements']['b']['names'],comparison['array_elements']['a_and_b']['names'])
+    print(f"Here is the overlap coefficient for names: {overlap_coefficient_names}")
+
+
+
+
