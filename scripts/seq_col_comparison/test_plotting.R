d9435e678488ca0e6681eab8964229d5bdf98888
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-05-30 09:08:26 -0600

    attempt 2 in 1 heatmap

--- scripts/seq_col_comparison/test_plotting.R
+++ scripts/seq_col_comparison/test_plotting.R
@@ -11,7 +11,8 @@ library(stringr)
 #results_dir <- "/home/drc/Downloads/refgenomes_pics_test/12May2025/ncbi_grch38_patches/"
 #results_dir <- "/home/drc/Downloads/refgenomes_pics_test/12May2025/ensembl1/"
 #results_dir <- "/home/drc/Downloads/refgenomes_pics_test/12May2025/ncbi_grch38_patches/patches_with_svgs/"
-results_dir <- "/home/drc/Downloads/refgenomes_pics_test/28May2025/homo_sapiens/hg19p13/"
+#results_dir <- "/home/drc/Downloads/refgenomes_pics_test/28May2025/homo_sapiens/hg19p13/"
+results_dir <- "/home/drc/Downloads/refgenomes_pics_test/28May2025/homo_sapiens/4_comparisons/"
 
 if (!dir.exists(results_dir)) {
   stop(paste("Error: Directory does not exist:", results_dir))
@@ -102,66 +103,66 @@ for (csv_file in csv_files) {
   title_cased_stat_name <- str_to_title(cleaned_stat_name)
 
   # Save the heatmap (optional)
-  tryCatch({
-  pheatmap(
-    mat = heatmap_matrix_ordered,
-    color = my_color(100),
-    breaks = my_breaks,
-    border_color = NA,
-    cluster_rows = FALSE,            # Do not cluster rows
-    cluster_cols = FALSE,            # Do not cluster columns
-    order_rows = match(ordered_samples, rownames(heatmap_matrix_ordered)), # Order rows by authority
-    order_cols = match(ordered_samples, colnames(heatmap_matrix_ordered)), # Order columns by authority
-    annotation_row = annotation_row,
-    annotation_col = annotation_col, # Add column annotation
-    annotation_colors = list(Authority = authority_colors),
-     #annotation_position = c("row", "bottom"),
-    show_rownames = TRUE,
-    show_colnames = TRUE,
-    main = paste("Heatmap of", title_cased_stat_name),
-    fontsize = 6,
-    fontsize_row = 6,
-    fontsize_col = 6,
-    cellheight = 20,
-    cellwidth = 20,
-  )
-    dev.copy(png, file.path(results_dir, paste0("pheatmap_", stat_name, ".png")), width = 1200, height = 1200, res = 300)
-    # Create the heatmap with pheatmap, with ordered samples and column annotation
-    dev.off()
-    message(paste("Successfully saved plot:", stat_name))
-  }, error = function(e) {
-    warning(paste("Error saving plot:", stat_name, "\n", e$message))
-  })
+  # tryCatch({
+  # pheatmap(
+  #   mat = heatmap_matrix_ordered,
+  #   color = my_color(100),
+  #   breaks = my_breaks,
+  #   border_color = NA,
+  #   cluster_rows = FALSE,            # Do not cluster rows
+  #   cluster_cols = FALSE,            # Do not cluster columns
+  #   order_rows = match(ordered_samples, rownames(heatmap_matrix_ordered)), # Order rows by authority
+  #   order_cols = match(ordered_samples, colnames(heatmap_matrix_ordered)), # Order columns by authority
+  #   annotation_row = annotation_row,
+  #   annotation_col = annotation_col, # Add column annotation
+  #   annotation_colors = list(Authority = authority_colors),
+  #    #annotation_position = c("row", "bottom"),
+  #   show_rownames = TRUE,
+  #   show_colnames = TRUE,
+  #   main = paste("Heatmap of", title_cased_stat_name),
+  #   fontsize = 6,
+  #   fontsize_row = 6,
+  #   fontsize_col = 6,
+  #   cellheight = 20,
+  #   cellwidth = 20,
+  # )
+  #   dev.copy(png, file.path(results_dir, paste0("pheatmap_", stat_name, ".png")), width = 1200, height = 1200, res = 300)
+  #   # Create the heatmap with pheatmap, with ordered samples and column annotation
+  #   dev.off()
+  #   message(paste("Successfully saved plot:", stat_name))
+  # }, error = function(e) {
+  #   warning(paste("Error saving plot:", stat_name, "\n", e$message))
+  # })
 
   # Save the heatmap as SVG
-  # tryCatch({
-  #   svg(file.path(results_dir, paste0("pheatmap_", stat_name, ".svg")), width = 8, height = 8) # Adjust width and height as needed
-  #   pheatmap(
-  #     mat = heatmap_matrix_ordered,
-  #     color = my_color(100),
-  #     breaks = my_breaks,
-  #     border_color = NA,
-  #     cluster_rows = FALSE,            # Do not cluster rows
-  #     cluster_cols = FALSE,            # Do not cluster columns
-  #     order_rows = match(ordered_samples, rownames(heatmap_matrix_ordered)), # Order rows by authority
-  #     order_cols = match(ordered_samples, colnames(heatmap_matrix_ordered)), # Order columns by authority
-  #     annotation_row = annotation_row,
-  #     annotation_col = annotation_col, # Add column annotation
-  #     annotation_colors = list(Authority = authority_colors),
-  #      #annotation_position = c("row", "bottom"),
-  #     show_rownames = TRUE,
-  #     show_colnames = TRUE,
-  #     main = paste("Heatmap of", title_cased_stat_name),
-  #     fontsize = 6,
-  #     fontsize_row = 6,
-  #     fontsize_col = 6,
-  #     cellheight = 7,
-  #     cellwidth = 7
-  #   )
-  #   dev.off()
-  #   message(paste("Successfully saved SVG plot:", stat_name))
-  # }, error = function(e) {
-  #   warning(paste("Error saving SVG plot:", stat_name, "\n", e$message))
-  # })
+  tryCatch({
+    svg(file.path(results_dir, paste0("pheatmap_", stat_name, ".svg")), width = 8, height = 8) # Adjust width and height as needed
+    pheatmap(
+      mat = heatmap_matrix_ordered,
+      color = my_color(100),
+      breaks = my_breaks,
+      border_color = NA,
+      cluster_rows = FALSE,            # Do not cluster rows
+      cluster_cols = FALSE,            # Do not cluster columns
+      order_rows = match(ordered_samples, rownames(heatmap_matrix_ordered)), # Order rows by authority
+      order_cols = match(ordered_samples, colnames(heatmap_matrix_ordered)), # Order columns by authority
+      annotation_row = annotation_row,
+      annotation_col = annotation_col, # Add column annotation
+      annotation_colors = list(Authority = authority_colors),
+       #annotation_position = c("row", "bottom"),
+      show_rownames = TRUE,
+      show_colnames = TRUE,
+      main = paste("Heatmap of", title_cased_stat_name),
+      fontsize = 6,
+      fontsize_row = 6,
+      fontsize_col = 6,
+      cellheight = 7,
+      cellwidth = 7
+    )
+    dev.off()
+    message(paste("Successfully saved SVG plot:", stat_name))
+  }, error = function(e) {
+    warning(paste("Error saving SVG plot:", stat_name, "\n", e$message))
+  })
 
 }
