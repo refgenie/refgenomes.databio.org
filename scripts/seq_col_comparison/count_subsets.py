0b097564a5adeecbc258cc5b2d4e6801124b8d2a
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-06-10 14:58:31 -0600

    add calculating subsets based on OPA and OPB values

--- scripts/seq_col_comparison/count_subsets.py
+++ scripts/seq_col_comparison/count_subsets.py
@@ -6,6 +6,8 @@ from pephubclient import PEPHubClient
 import pipestat
 
 RESULTS_PEP= "donaldcampbelljr/human_seq_col_results:default"
+#RESULTS_PEP= "donaldcampbelljr/mouse_seq_col_results:default"
+COMPARISON = 1.0 
 
 #results_pep = "donaldcampbelljr/test_seq_col_results:default"
 #results_pep = sys.argv[1] # input pep for graphing
@@ -18,47 +20,49 @@ pep = phc.load_project(RESULTS_PEP)
 #print(pep["_sample_df"])
 
 pep_df = pep["_sample_df"]
-
-# # Initialize lists to store sample names based on 'jaccard_name_len' condition
-# samples_opa_name_len_in_col1 = []
-# samples_opb_name_len_in_col2 = []
-
-# # opa_name_len
-# # opb_name_len
-
-# # Iterate through the DataFrame rows
-# for index, row in pep_df.iterrows():
-#     if row['sample_name_1'] == row['sample_name_2']:
-#         print("sample names the same, passing")
-#         pass
-#     else:
-#         if row['opa_name_len'] == 1.0:
-#             samples_opa_name_len_in_col1.append(row['sample_name_1'])
-#         if row['opb_name_len'] == 1.0:
-#             samples_opb_name_len_in_col2.append(row['sample_name_2'])
-
-# # Convert lists to sets to get unique sample names
-# unique_samples_col1 = set(samples_opa_name_len_in_col1)
-# unique_samples_col2 = set(samples_opb_name_len_in_col2)
-
-# all_col_sample = unique_samples_col1.union(unique_samples_col2)
-
-# # Get all unique sample names from 'sample_name_1' and 'sample_name_2' columns
 all_unique_samples = set(pep_df['sample_name_1']).union(set(pep_df['sample_name_2']))
 
-# #print(f"Samples where 'opa_name_len' is 1 (from sample_name_1 column): {unique_samples_col1}")
-# #print(f"Samples where 'opb_name_len' is 1 (from sample_name_2 column): {unique_samples_col2}")
-# #print(f"All unique samples in the DataFrame: {all_unique_samples}")
+# Initialize lists to store sample names based on 'jaccard_name_len' condition
+samples_opa_name_len_in_col1 = []
+samples_opb_name_len_in_col2 = []
 
 
-# print(f"LEN unique_samples_col1: {len(unique_samples_col1)}")
-# print(f"LEN unique_samples_col2: {len(unique_samples_col2)}")
-# print(f"LEN union: {len(all_col_sample)}")
-# print(f"LEN All Unique Samples: {len(all_unique_samples)}")
+# PICK OPA/OPB STats
+STAT_NAME = "name_len"
+stat1 = "opa_name_len"
+stat2 = "opb_name_len"
 
-# for sample in all_col_sample:
-#     print(sample)
+stats = [("opa_name_len","opb_name_len", "name_len"), ("opa_lengths","opb_lengths", "lengths"), ("opa_sequences","opb_sequences", "sequences"), ("opa_names","opb_names", "names")]
 
+# Iterate through the DataFrame rows
+for stat1, stat2, stat3 in stats:
+    for index, row in pep_df.iterrows():
+        if row['sample_name_1'] == row['sample_name_2']:
+            print("sample names the same, passing")
+            pass
+        else:
+            if row[stat1] == 1.0:
+                samples_opa_name_len_in_col1.append(row['sample_name_1'])
+            if row[stat2] == 1.0:
+                samples_opb_name_len_in_col2.append(row['sample_name_2'])
+
+    # Convert lists to sets to get unique sample names
+    unique_samples_col1 = set(samples_opa_name_len_in_col1)
+    unique_samples_col2 = set(samples_opb_name_len_in_col2)
+
+    all_col_sample = unique_samples_col1.union(unique_samples_col2)
+
+    print(f"LEN unique_samples_col1: {len(unique_samples_col1)}")
+    print(f"LEN unique_samples_col2: {len(unique_samples_col2)}")
+    print(f"LEN union: {len(all_col_sample)}")
+    print(f"LEN All Unique Samples: {len(all_unique_samples)}")
+    print(f"Percentage for OPA/OPB for {stat3} = {(len(all_col_sample)/len(all_unique_samples))*100}")
+
+    # for sample in all_col_sample:
+    #     print(sample)
+
+
+# ------------- Calculating Percentages based on Jaccard SImialrity for Each Stat
 all_relevant_stats = [
                       'jaccard_names', 
                       'jaccard_lengths', 
@@ -80,7 +84,7 @@ for stat in all_relevant_stats:
             print("sample names the same, passing")
             pass
         else:
-            if row[stat] == 1.0:
+            if row[stat] >= COMPARISON: # CHECK TO ENSURE THIS IS THE PROPER COMPARISON IF CHANGING NUMBER
                 count_target+=1
                 samples_jaccard_name_len.append(row['sample_name_1'])
                 samples_jaccard_name_len.append(row['sample_name_2'])
@@ -92,10 +96,10 @@ for stat in all_relevant_stats:
     # print(count_all)
     # print(count_target)
 
-    print(f"here is how many {stat} were equal to 1.0 divided by all comparisons")
+    print(f"here is how many {stat} were equal to {COMPARISON} divided by all comparisons")
     print((count_target/count_all)*100)
 
-    print(f"here is how many samples were equal to 1.0 divided by all total number of samples for stat {stat}")
+    print(f"here is how many samples were equal to {COMPARISON} divided by all total number of samples for stat {stat}")
     print((len(unique_samples_name_len)/len(all_unique_samples))*100)
 
 
