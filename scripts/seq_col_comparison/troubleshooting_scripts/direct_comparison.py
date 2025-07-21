0cc85ad5565502a67b107b760a0135594fb07774
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2025-07-21 15:58:53 -0600

    sample comparisons updated for manual example

--- scripts/seq_col_comparison/troubleshooting_scripts/direct_comparison.py
+++ scripts/seq_col_comparison/troubleshooting_scripts/direct_comparison.py
@@ -18,9 +18,46 @@ LOCAL_JSON_DIRECTORY = "/home/drc/Downloads/jsons_from_rivanna/json/"
 # samples_to_compare = ["XemD97fxYMS4q-FBm_n5CHQgmzh1_67a"] # p0
 # primary_sample = "u1HyLgIlq8M_XvEwy0oGqAvKGHJMGtxH" # p14
 
-samples_to_compare = ["6pxZqxG0TYtyVb8yp14ONpaxZ8msQqKr"] # hg19-p13-plusMT-ucsc
-primary_sample = "svwHqvgassl0loigdqVIQJdjo7NWDIx3" # hg19-p13-no-alt-analysis-ucsc
+samples_to_compare = ["a_WL8OC7sFJfjux5m11M2bKl0dYepA1x"] 
+primary_sample = "tkRdBlSp5hewK4OpEJC87J9pw-ac0vOa" 
 
+
+# lWRRNMNypacEjnJCy-AYiDNUPy1brQGC
+# a_WL8OC7sFJfjux5m11M2bKl0dYepA1x
+
+# def calc_jaccard_similarities(A: dict, B: dict) -> dict:
+#     """
+#     Takes two sequence collections and calculates jaccard similarties for all attributes
+
+#     @param A Sequence collection A
+#     @param B Sequence collection B
+#     @return dict jaccard similarities for all attributes
+#     """
+
+#     def calc_jaccard_similarity(A_B_intersection, A_B_union):
+#         if A_B_union == 0:
+#             return 0.0
+#         jaccard_similarity = A_B_intersection / A_B_union
+#         return jaccard_similarity
+
+#     jaccard_similarities = {}
+
+#     comparison_dict = compare_seqcols(A,B)
+
+#     list_a_keys = list(comparison_dict['array_elements']['a_and_b'].keys())
+
+#     for key in list_a_keys:
+
+#         intersection_seqcol = comparison_dict['array_elements']['a_and_b'].get(key)
+
+#         a = comparison_dict['array_elements']['a'].get(key)
+#         b = comparison_dict['array_elements']['b'].get(key)
+
+#         if a and b and intersection_seqcol:
+#             union_seqcol =  a + b - intersection_seqcol # inclusion-exclusion principal for calculating union
+#             jaccard_similarity = calc_jaccard_similarity(intersection_seqcol,union_seqcol)
+#             jaccard_similarities.update({key: jaccard_similarity})
+#     return jaccard_similarities
 
 def get_sequence_col_dict(digest):
 
@@ -60,6 +97,11 @@ for sample in samples_to_compare:
     reloaded_dict2 = get_sequence_col_dict(primary_sample)
     # print(pprint(compare_seqcols(reloaded_dict1,reloaded_dict2),indent=4))
     comparison = compare_seqcols(reloaded_dict1,reloaded_dict2)
+    print(comparison)
+    # calculated_jaccards = calc_jaccard_similarities(reloaded_dict1,reloaded_dict2)
+    # print(calculated_jaccards)
+    # print(len(reloaded_dict1['names']))
+    # print(len(reloaded_dict2['names']))
 
     # create set of name_length_pairs
         #print(type(reloaded_dict1['name_length_pairs'][0]))
